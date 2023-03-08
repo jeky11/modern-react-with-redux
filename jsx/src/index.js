9a840f59be34f91367aa1228323726958d69b32b
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-03-08 23:31:18 +0700

    input and textarea

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -5,7 +5,14 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 function App() {
-    return <h1>Hi there!</h1>;
+    let minvalue = 5;
+    return (
+        <div>
+            <input type="number" min={minvalue}/>
+            <br/><br/>
+            <textarea autoFocus={true}></textarea>
+        </div>
+    );
 }
 
 root.render(<App/>);
