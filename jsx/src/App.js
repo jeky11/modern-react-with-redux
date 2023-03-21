f67d615c886686a546c6bcfbe21adbcc778d9213
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-03-21 18:43:01 +0800

    first component

---
+++ jsx/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+    return <h1>He there!!</h1>;
+}
+
+export default App;
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App'
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
-
-function App() {
-    let minvalue = 5;
-    return (
-        <div>
-            <input type="number" min={minvalue}/>
-            <br/><br/>
-            <textarea autoFocus={true}></textarea>
-        </div>
-    );
-}
 
 root.render(<App/>);
