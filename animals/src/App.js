58ef6dd2584195045f6726abc7e754ee2fdd39d4
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-03-30 20:14:19 +0800

    count state

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,13 +1,17 @@
+import {useState} from "react";
 import AnimalShow from "./AnimalShow";
 
 function App() {
+    const [count, setCount] = useState(0);
+
     const handleClick = () => {
-        console.log('Button was clicked')
+        setCount(count + 1)
     };
 
     return (
         <div>
             <button onClick={handleClick}>Add Animal</button>
+            <div>Number of animals: {count}</div>
         </div>
     );
 }
