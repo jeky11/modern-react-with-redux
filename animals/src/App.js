ed35db5923c9ed6c2b4e508bba3af9b98abaedce
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-03-31 19:28:04 +0800

    render animal component

--- animals/src/App.js
+++ animals/src/App.js
@@ -15,10 +15,14 @@ function App() {
         setAnimals([...animals, randomAnimal])
     };
 
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index}/>
+    });
+
     return (
         <div>
             <button onClick={handleClick}>Add Animal</button>
-            <div>{animals}</div>
+            <div>{renderedAnimals}</div>
         </div>
     );
 }
