d053d065ec9b70d8bd031204a9c5ef39ae82bd4e
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-04-03 20:39:46 +0800

    search images func

---
+++ pics/src/api.js
@@ -0,0 +1,14 @@
+﻿import axios from "axios";
+
+const searchImages = async () => {
+    return await axios.get('https://api.unsplash.com/search/photos', {
+        headers: {
+            Authorization: "Client-ID i4YLjl7eRO_1YTICFwjzfHasr7wbI8LfagA8ZV-im24"
+        },
+        params: {
+            query: 'cars'
+        }
+    });
+};
+
+export default searchImages;
