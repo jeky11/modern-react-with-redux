e42eccf654eacf338894dce38682f063ae5bde6c
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-05-03 23:34:59 +0300

    fix bug for useEffect by adding useCallback

--- books/src/App.js
+++ books/src/App.js
@@ -8,7 +8,7 @@ function App() {
 
     useEffect(() => {
         fetchBooks(); // must be used without await
-    }, []);
+    }, [fetchBooks]);
 
     return (
         <div className="app">
--- books/src/context/books.js
+++ books/src/context/books.js
@@ -1,4 +1,4 @@
-import {createContext, useState} from "react";
+import {createContext, useState, useCallback} from "react";
 import axios from "axios";
 
 const BooksContext = createContext();
@@ -6,17 +6,17 @@ const BooksContext = createContext();
 function Provider({children}) {
     const [books, setBooks] = useState([]);
 
-    const fetchBooks = async () => {
+    const fetchBooks = useCallback(async () => {
         const response = await axios.get('http://localhost:3001/books');
         setBooks(response.data);
-    }
+    }, []);
 
     const createBook = async (title) => {
         const response = await axios.post('http://localhost:3001/books', {title});
 
         const updatedBooks = [...books, response.data];
         setBooks(updatedBooks);
-    }
+    };
 
     const editBookById = async (id, newTitle) => {
         const response = await axios.put(`http://localhost:3001/books/${id}`, {title: newTitle});
@@ -30,7 +30,7 @@ function Provider({children}) {
         });
 
         setBooks(updatedBooks);
-    }
+    };
 
     const deleteBookById = async (id) => {
         await axios.delete(`http://localhost:3001/books/${id}`);
@@ -40,7 +40,7 @@ function Provider({children}) {
         });
 
         setBooks(updatedBooks);
-    }
+    };
 
     const valueToShare = {
         books,
