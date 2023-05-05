0ffda8ee8bd8944879fcb8c3c88d7bc58174f734
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-05-05 19:47:11 +0300

    new structure

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,41 +1,9 @@
-import {GoBell, GoCloudDownload, GoDatabase} from "react-icons/go";
-import Button from "./Button";
+
 
 function App() {
-    const handleClick = () => {
-        console.log("click!");
-    };
-
     return (
         <div>
-            <div>
-                <Button primary rounded outline className="mb-5" onClick={handleClick}>
-                    <GoBell/>
-                    Click me!
-                </Button>
-            </div>
-            <div>
-                <Button secondary outline>
-                    <GoCloudDownload/>
-                    Button 2
-                </Button>
-            </div>
-            <div>
-                <Button success>
-                    <GoDatabase/>
-                    Hello
-                </Button>
-            </div>
-            <div>
-                <Button warning outline>
-                    Cancel
-                </Button>
-            </div>
-            <div>
-                <Button danger rounded>
-                    Oh no!
-                </Button>
-            </div>
+            
         </div>
     );
 }
---
+++ comps/src/pages/ButtonPage.js
@@ -0,0 +1,43 @@
+﻿import {GoBell, GoCloudDownload, GoDatabase} from "react-icons/go";
+import Button from "../components/Button";
+
+function ButtonPage() {
+    const handleClick = () => {
+        console.log("click!");
+    };
+
+    return (
+        <div>
+            <div>
+                <Button primary rounded outline className="mb-5" onClick={handleClick}>
+                    <GoBell/>
+                    Click me!
+                </Button>
+            </div>
+            <div>
+                <Button secondary outline>
+                    <GoCloudDownload/>
+                    Button 2
+                </Button>
+            </div>
+            <div>
+                <Button success>
+                    <GoDatabase/>
+                    Hello
+                </Button>
+            </div>
+            <div>
+                <Button warning outline>
+                    Cancel
+                </Button>
+            </div>
+            <div>
+                <Button danger rounded>
+                    Oh no!
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+export default ButtonPage;
