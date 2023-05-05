26aa3b060b79c2aef40ab20fe483250881b52680
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-05-05 21:52:24 +0300

    text for accordion

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -1,6 +1,15 @@
 function Accordion({items}) {
+    const renderedItems = items.map((item) => {
+        return (
+            <div key={item.id}>
+                <div>{item.label}</div>
+                <div>{item.content}</div>
+            </div>
+        )
+    });
+
     return (
-        <div>Acc</div>
+        <div>{renderedItems}</div>
     );
 }
 
