1d6a904b9d4cdde619d955524317186b3e45b005
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-05-09 18:57:57 +0300

    icon for accordion

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import {GoChevronDown, GoChevronLeft} from "react-icons/go";
 
 function Accordion({items}) {
     const [expendedIndex, setExpendedIndex] = useState(0);
@@ -6,9 +7,16 @@ function Accordion({items}) {
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expendedIndex;
 
+        const icon = <span>
+            {isExpanded ? <GoChevronDown/> : <GoChevronLeft/>}
+        </span>
+
         return (
             <div key={item.id}>
-                <div onClick={() => setExpendedIndex(index)}>{item.label}</div>
+                <div onClick={() => setExpendedIndex(index)}>
+                    {icon}
+                    {item.label}
+                </div>
                 {isExpanded && <div>{item.content}</div>}
             </div>
         )
