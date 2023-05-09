8ca3ffbf085407a2a9c38ff4fa3deba9ed7b3eb3
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-05-09 21:15:00 +0300

    is open state

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,4 +1,12 @@
-﻿function Dropdown({options}) {
+﻿import {useState} from "react";
+
+function Dropdown({options}) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    }
+
     const renderedOptions = options.map(option => {
         return <div key={option.value}>
             {option.label}
@@ -7,8 +15,8 @@
 
     return (
         <div>
-            <div>Select...</div>
-            <div>{renderedOptions}</div>
+            <div onClick={handleClick}>Select...</div>
+            {isOpen && <div>{renderedOptions}</div>}
         </div>
     );
 }
