dc2826d27f6138f93d2df50a2a4f36ae75c47eab
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-05-15 22:35:58 +0300

    finish modal

--- comps/src/pages/ModalPage.js
+++ comps/src/pages/ModalPage.js
@@ -13,10 +13,124 @@ function ModalPage() {
         setShowModal(false);
     };
 
+    const actionBar = (
+        <div>
+            <Button primary onClick={handleClose}>I Accept</Button>
+        </div>
+    );
+
+    const modal = (
+        <Modal onClose={handleClose} actionBar={actionBar}>
+            <p> Here is and important agreement for you to accept</p>
+        </Modal>
+    );
+
     return (
         <div>
             <Button primary onClick={handleClick}>Open Modal</Button>
-            {showModal && <Modal onClose={handleClose}/>}
+            {showModal && modal}
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi faucibus venenatis enim, non luctus neque
+                ullamcorper quis. Suspendisse vitae mollis felis. Praesent quis risus eu justo fringilla suscipit. Proin
+                sit amet est iaculis, commodo nisl eu, feugiat magna. Nullam maximus urna sed efficitur condimentum.
+                Vestibulum sit amet feugiat diam, ac dignissim dui. Pellentesque placerat auctor nulla, sed vulputate
+                metus elementum et. Proin posuere, sapien vel venenatis consequat, tellus nisl vulputate metus, ac
+                vehicula neque tortor eu libero.</p>
         </div>
     );
 }
