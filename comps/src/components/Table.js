d53ec83dda238abf0c509f4acec09d472835c18e
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-05-16 18:59:46 +0300

    table without sorting

--- comps/src/components/Table.js
+++ comps/src/components/Table.js
@@ -1,12 +1,22 @@
-function Table({data}) {
-    const renderedRows = data.map((fruit) => {
+function Table({data, config, keyFn}) {
+    const renderedHeaders = config.map((column) => {
         return (
-            <tr key={fruit.name} className='border-b'>
-                <td className='p-3'>{fruit.name}</td>
-                <td className='p-3'>
-                    <div className={`p-3 m-2 ${fruit.color}`}></div>
+            <th key={column.label}>{column.label}</th>
+        );
+    });
+
+    const renderedRows = data.map((rowData) => {
+        const renderedCells = config.map((column) => {
+            return (
+                <td key={column.label} className='p-3'>
+                    {column.render(rowData)}
                 </td>
-                <td className='p-3'>{fruit.score}</td>
+            );
+        });
+
+        return (
+            <tr key={keyFn(rowData)} className='border-b'>
+                {renderedCells}
             </tr>
         );
     });
@@ -15,9 +25,7 @@ function Table({data}) {
         <table className='table-auto border-spacing-2'>
             <thead>
             <tr className='border-b-2'>
-                <th>Fruit</th>
-                <th>Color</th>
-                <th>Score</th>
+                {renderedHeaders}
             </tr>
             </thead>
             <tbody>
