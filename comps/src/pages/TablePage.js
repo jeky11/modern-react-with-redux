6d2884a2e8c12db313ae384a74b46011b122abbd
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-05-18 18:57:41 +0300

    finish table component

--- comps/src/pages/TablePage.js
+++ comps/src/pages/TablePage.js
@@ -19,12 +19,13 @@ function TablePage() {
         },
         {
             label: 'Score',
-            render: (fruit) => fruit.score
-        },
+            render: (fruit) => fruit.score,
+            header: () => <th className="bg-red-500">Score</th>
+        }
     ];
-    
+
     const keyFn = (fruit) => {
-      return fruit.name;
+        return fruit.name;
     };
 
     return (
