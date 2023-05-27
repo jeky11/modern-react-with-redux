f2c66dcfb1b6cb2a66eb9b5b225041955838471e
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-05-27 16:21:50 +0300

    show car list

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -1,7 +1,32 @@
-﻿function CarList() {
+﻿import {useDispatch, useSelector} from "react-redux";
+import {removeCar} from "../store";
+
+function CarList() {
+    const dispatch = useDispatch();
+    const cars = useSelector((state) => state.cars.data);
+
+    const handleCarDelete = (car) => {
+        dispatch(removeCar(car.id));
+    };
+
+    const renderedCars = cars.map(car => {
+        return (
+            <div key={car.id} className="panel">
+                <p>
+                    {car.name} - ${car.cost}
+                </p>
+                <button className="button is-danger"
+                        onClick={() => handleCarDelete(car)}>
+                    Delete
+                </button>
+            </div>
+        )
+    });
+
     return (
-        <div>
-            CarList
+        <div className="car-list">
+            {renderedCars}
+            <hr/>
         </div>
     );
 }
