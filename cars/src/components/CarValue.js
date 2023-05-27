7c4738416a00690f4b84c889cf4f8daac387d8c8
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-05-27 19:15:04 +0300

    car value

--- cars/src/components/CarValue.js
+++ cars/src/components/CarValue.js
@@ -1,7 +1,17 @@
+import {useSelector} from "react-redux";
+
 function CarValue() {
+    const totalCost = useSelector(({cars: {data, searchTerm}}) => {
+        const filteredCars = data.filter((car) =>
+            car.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+
+        return filteredCars.reduce((acc, car) => acc + car.cost, 0);
+    });
+
     return (
-        <div>
-            CarValue
+        <div className="car-value">
+            Total Cost: ${totalCost}
         </div>
     );
 }
