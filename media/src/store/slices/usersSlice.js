6f5297cd4dca558aa3adfc70e4bb02edf26f2480
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-06-02 19:10:15 +0300

    initial redux setup

---
+++ media/src/store/slices/usersSlice.js
@@ -0,0 +1,11 @@
+﻿import {createSlice} from "@reduxjs/toolkit";
+
+const usersSlice = createSlice({
+    name: 'users',
+    initialState: {
+        data: []
+    },
+    reducers: {}
+});
+
+export const usersReducer = usersSlice.reducer;
