0f3879e2e7cbd8fc48c87b2b9b5509922be34c2e
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-06-02 19:11:26 +0300

    remove bom

--- media/src/store/index.js
+++ media/src/store/index.js
@@ -1,4 +1,4 @@
-﻿import {configureStore} from "@reduxjs/toolkit";
+import {configureStore} from "@reduxjs/toolkit";
 import {usersReducer} from "./slices/usersSlice";
 
 export const store = configureStore({
