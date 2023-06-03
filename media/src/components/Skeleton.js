c033b13c241bc23a47b06561cf1cfedc009ddfeb
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-06-03 15:24:16 +0300

    blank skeleton

---
+++ media/src/components/Skeleton.js
@@ -0,0 +1,11 @@
+﻿function Skeleton({times}) {
+    const boxes = [];
+
+    for (let i = 0; i < times; i++) {
+        boxes.push(<div key={i}/>);
+    }
+
+    return boxes;
+}
+
+export default Skeleton;
