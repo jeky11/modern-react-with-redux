0422a2255ce7a6a63b5c045e253831f8f603af36
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-06-03 16:14:18 +0300

    animation for loading

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -14,11 +14,11 @@ function UsersList() {
     }, [dispatch]);
 
     if (isLoading) {
-        return <Skeleton times={6}/>
+        return <Skeleton times={6} className='h-10 w-full'/>;
     }
 
     if (error) {
-        return <div>Error fetching data...</div>
+        return <div>Error fetching data...</div>;
     }
 
     return (
